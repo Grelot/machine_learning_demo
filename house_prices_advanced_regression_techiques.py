6bfb83d39958062bc33a8bd4c8788c1e1dad17ee
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2020-03-02 16:58:16 +0100

    first part house prices

---
+++ house_prices_advanced_regression_techiques.py
@@ -0,0 +1,125 @@
+#===============================================================================
+#INFORMATION
+#===============================================================================
+# Codes for Predicting Housing Prices:
+# A model to predict the value of a given house in the Boston real estate market
+# using various statistical analysis tools.
+# Identified the best price that a client can sell their house 
+# using machine learning.
+# 
+# Guerin Pierre-Edouard
+#
+#
+# git repository : https://github.com/Grelot/machine_learning_demo
+#
+#==============================================================================
+#NOTICE
+#==============================================================================
+#
+#
+#
+#
+#==============================================================================
+#MODULES
+#==============================================================================
+import argparse
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from collections import Counter
+
+from sklearn.linear_model import LinearRegression,LassoCV, Ridge, LassoLarsCV,ElasticNetCV
+from sklearn.model_selection import GridSearchCV, cross_val_score, learning_curve
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, ExtraTreesRegressor, GradientBoostingRegressor
+from sklearn.preprocessing import StandardScaler, Normalizer, RobustScaler
+import scipy.stats as stats
+import xgboost as xgb
+import lightgbm as lgb
+import warnings
+
+
+warnings.filterwarnings('ignore')
+sns.set(style='white', context='notebook', palette='deep')
+
+#==============================================================================
+#CLASS
+#==============================================================================
+
+
+
+#==============================================================================
+#FUNCTIONS
+#==============================================================================
+
+def multiplot(data,features,plottype,nrows,ncols,figsize,y=None,colorize=False):
+    """ This function draw a multi plot for 3 types of plots ["regplot","distplot","coutplot"]"""
+    n = 0
+    plt.figure(1)
+    fig, axes = plt.subplots(nrows, ncols, figsize=figsize)
+    
+    if colorize:
+        colors = sns.color_palette(n_colors=(nrows*ncols))
+    else :
+        colors = [None]*(nrows*ncols)
+        
+    for row in range(ncols):
+        for col in range(nrows):
+            
+            if plottype == 'regplot':
+                if y == None:
+                    raise ValueError('y value is needed with regplot type')
+                
+                sns.regplot(data = data, x = features[n], y = y ,ax=axes[row,col], color = colors[n])
+                correlation = np.corrcoef(data[features[n]],data[y])[0,1]
+                axes[row,col].set_title("Correlation {:.2f}".format(correlation))
+            
+            elif plottype == 'distplot':
+                sns.distplot(a = data[features[n]],ax = axes[row,col],color=colors[n])
+                skewness = data[features[n]].skew()
+                axes[row,col].legend(["Skew : {:.2f}".format(skewness)])
+            
+            elif plottype in ['countplot']:
+                g = sns.countplot(x = data[features[n]], y = y, ax = axes[row,col],color = colors[n])
+                g = plt.setp(g.get_xticklabels(), rotation=45)
+                
+            n += 1
+    plt.tight_layout()
+    plt.show()
+    plt.gcf().clear()
+
+#==============================================================================
+#MAIN
+#==============================================================================
+
+
+# download data https://www.kaggle.com/c/house-prices-advanced-regression-techniques/data
+
+trainDatFile="data/house_prices_advanced_regression_techniques/train.csv"
+testDatFile="data/house_prices_advanced_regression_techniques/test.csv"
+
+train = pd.read_csv(trainDatFile)
+test = pd.read_csv(testDatFile)
+
+# number of descripteurs
+train.shape
+
+# variables quantitatives
+train.select_dtypes(include=['int64','float64']).columns
+
+# drop useless "Id" feature
+train = train.drop(labels = ["Id"],axis = 1)
+test = test.drop(labels = ["Id"],axis = 1)
+
+
+# Let's explore the data.
+g = sns.jointplot(x = train['GrLivArea'], y = train['SalePrice'],kind="reg")
+g.annotate(stats.pearsonr)
+
+
+# explatory analysis sale price
+train['SalePrice'].describe()
+
+g = sns.distplot(train['SalePrice'],color="gray")
+g = g.legend(["Coefficient Assymetrie : {:.2f}".format(train['SalePrice'].skew())],loc='best')
