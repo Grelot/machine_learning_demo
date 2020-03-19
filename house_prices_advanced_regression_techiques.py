5e5b6d152545c928b0e8adf6aec68811002ca6f8
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2020-03-19 23:40:42 +0100

    part 5 debug + done

--- house_prices_advanced_regression_techiques.py
+++ house_prices_advanced_regression_techiques.py
@@ -33,7 +33,7 @@ from collections import Counter
 from sklearn.linear_model import LinearRegression,LassoCV, Ridge, LassoLarsCV,ElasticNetCV
 from sklearn.model_selection import GridSearchCV, cross_val_score, learning_curve
 from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, ExtraTreesRegressor, GradientBoostingRegressor
-from sklearn.preprocessing import StandardScaler, Normalizer, RobustScaler
+from sklearn.preprocessing import StandardScaler, Normalizer, RobustScaler, LabelEncoder
 import scipy.stats as stats
 import xgboost as xgb
 import lightgbm as lgb
@@ -167,9 +167,88 @@ g = sns.countplot(dataset["SaleType"])
 dataset["SaleType"] = dataset["SaleType"].fillna("WD")
 ## 
 Function_feat = ["Functional","Exterior2nd","Exterior1st","Electrical"]
-multiplot(data = dataset ,features = Function_feat,plottype = "countplot",nrows = 2, ncols = 2, figsize = (11,9), colorize = True)
+multiplot(data = dataset ,features = Function_feat, plottype = "countplot",nrows = 2, ncols = 2, figsize = (11,9), colorize = True)
 dataset["Functional"] = dataset["Functional"].fillna("Typ")
 dataset["Exterior2nd"] = dataset["Exterior2nd"].fillna("VinylSd")
 dataset["Exterior1st"] = dataset["Exterior1st"].fillna("VinylSd")
 dataset["Electrical"] = dataset["Electrical"].fillna("SBrkr")
 
+## categorical variables
+dataset = dataset.replace({'MSSubClass': {20: 'SubClass_20', 30: 'SubClass_30',40: 'SubClass_40',
+45: 'SubClass_45',50: 'SubClass_50',60: 'SubClass_60',70: 'SubClass_70',
+75: 'SubClass_75',80: 'SubClass_80',85: 'SubClass_85',90: 'SubClass_90',
+120: 'SubClass_120',150: 'SubClass_150',160: 'SubClass_160',180: 'SubClass_180',
+190: 'SubClass_190'}})
+dataset = dataset.replace({'MoSold': {1: 'Jan', 2: 'Feb',3: 'Mar',
+4: 'Apr',5: 'May',6: 'Jun',7: 'Jul',8: 'Aug',9: 'Sep',10: 'Oct',
+11: 'Nov',12: 'Dec'}})
+dataset['YrSold'] = dataset['YrSold'].astype(str)
+
+
+## Categorical values
+## Ordered
+cat_dtype = pd.api.types.CategoricalDtype(categories=['No','Po','Fa','TA','Gd','Ex'],ordered=True)
+dataset["BsmtCond"] = dataset["BsmtCond"].astype(cat_dtype)
+cat_dtype = pd.api.types.CategoricalDtype(categories=['No','Mn','Av','Gd'],ordered=True)
+dataset["BsmtExposure"] = dataset["BsmtExposure"].astype(cat_dtype)
+cat_dtype = pd.api.types.CategoricalDtype(categories=['No','Unf','LwQ','Rec','BLQ','ALQ','GLQ'],ordered=True)
+dataset["BsmtFinType1"] = dataset["BsmtFinType1"].astype(cat_dtype)
+dataset["BsmtFinType2"] = dataset["BsmtFinType2"].astype(cat_dtype)
+cat_dtype = pd.api.types.CategoricalDtype(categories=['No','Po','Fa','TA','Gd','Ex'],ordered=True)
+dataset["BsmtQual"] = dataset["BsmtQual"].astype(cat_dtype)
+cat_dtype = pd.api.types.CategoricalDtype(categories=['Po','Fa','TA','Gd','Ex'],ordered=True)
+dataset["ExterCond"] = dataset["ExterCond"].astype(cat_dtype)
+dataset["ExterQual"] = dataset["ExterQual"].astype(cat_dtype)
+cat_dtype = pd.api.types.CategoricalDtype(categories=['No','MnWw','GdWo','MnPrv','GdPrv'],ordered=True)
+dataset["Fence"] = dataset["Fence"].astype(cat_dtype)
+cat_dtype = pd.api.types.CategoricalDtype(categories=['No','Po','Fa','TA','Gd','Ex'],ordered=True)
+dataset["FireplaceQu"] = dataset["FireplaceQu"].astype(cat_dtype)
+cat_dtype = pd.api.types.CategoricalDtype(categories=['Sal','Sev','Maj2','Maj1','Mod','Min2','Min1','Typ'],ordered=True)
+dataset["Functional"] = dataset["Functional"].astype(cat_dtype)
+cat_dtype = pd.api.types.CategoricalDtype(categories=['No','Po','Fa','TA','Gd','Ex'],ordered=True)
+dataset["GarageCond"] = dataset["GarageCond"].astype(cat_dtype)
+dataset["GarageQual"] = dataset["GarageQual"].astype(cat_dtype)
+cat_dtype = pd.api.types.CategoricalDtype(categories=['No','Unf','RFn','Fin'],ordered=True)
+dataset["GarageFinish"] = dataset["GarageFinish"].astype(cat_dtype)
+cat_dtype = pd.api.types.CategoricalDtype(categories=['Po','Fa','TA','Gd','Ex'],ordered=True)
+dataset["HeatingQC"] = dataset["HeatingQC"].astype(cat_dtype)
+dataset["KitchenQual"] = dataset["KitchenQual"].astype(cat_dtype)
+cat_dtype = pd.api.types.CategoricalDtype(categories=['N','P','Y'],ordered=True)
+dataset["PavedDrive"] = dataset["PavedDrive"].astype(cat_dtype)
+cat_dtype = pd.api.types.CategoricalDtype(categories=['No','Fa','TA','Gd','Ex'],ordered=True)
+dataset["PoolQC"] = dataset["PoolQC"].astype(cat_dtype)
+cat_dtype = pd.api.types.CategoricalDtype(categories=['ELO','NoSeWa','NoSewr','AllPub'],ordered=True)
+dataset["Utilities"] = dataset["Utilities"].astype(cat_dtype)
+
+## non ordered
+dataset = pd.get_dummies(dataset, columns=["Alley","BldgType","CentralAir","Condition1","Condition2","Electrical","Exterior1st","Exterior2nd","Foundation","GarageType","Heating","HouseStyle","LandContour","LandSlope","LotConfig","LotShape","MSZoning","MasVnrType","MiscFeature","Neighborhood","RoofMatl","RoofStyle","SaleCondition","SaleType","Street","MSSubClass",'MoSold','YrSold'], drop_first=True)
+## ces variables sont colineaires
+dataset = dataset.drop(labels=['MSSubClass_SubClass_150','Condition2_PosN', 'MSSubClass_SubClass_160'],axis = 1)
+## print skewed features
+skewed_features = ["BsmtFinSF1","BsmtFinSF2","BsmtUnfSF","GarageArea","MasVnrArea", "TotalBsmtSF","1stFlrSF","2ndFlrSF","3SsnPorch","EnclosedPorch", "GrLivArea","LotArea","LowQualFinSF","OpenPorchSF","PoolArea","ScreenPorch","WoodDeckSF"]
+multiplot(data = dataset,features = skewed_features,plottype = "distplot", nrows = 4, ncols = 4, figsize = (11,9), colorize = True)
+
+## normalize
+skewed_features = ["BsmtFinSF1","BsmtFinSF2","BsmtUnfSF","GarageArea","MasVnrArea", "TotalBsmtSF","1stFlrSF","2ndFlrSF","3SsnPorch","EnclosedPorch", "GrLivArea","LotArea","LowQualFinSF","OpenPorchSF","PoolArea","ScreenPorch","WoodDeckSF"]
+for feature in skewed_features:
+    dataset[feature] = np.log1p(dataset[feature])
+multiplot(data = dataset,features = skewed_features,plottype = "distplot", nrows = 4, ncols = 4, figsize = (11,9), colorize = True)
+dataset["SalePrice"] = np.log1p(dataset["SalePrice"])
+Y = dataset["SalePrice"]
+dataset = dataset.drop(labels="SalePrice",axis = 1)
+
+features = dataset.columns
+
+LotF = dataset["LotFrontage"]
+dataset = dataset.drop(labels="LotFrontage",axis= 1)
+
+N = RobustScaler()
+
+N.fit(dataset)
+dataset = N.transform(dataset)
+
+## modelization
+
+## Separate train dataset and test dataset
+X_train = dataset[:train_len]
+test = dataset[train_len:]
